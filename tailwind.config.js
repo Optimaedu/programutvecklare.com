048cb19470e940ddc0a458aefeae71844e18edbf
WoIfey <wuflerz@outlook.com>
2023-04-13 10:56:25 +0300

    Init
    Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat",
+      },
+      backgroundImage: {
+        custom: ["-webkit-linear-gradient(#ffffff, #000000)"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
